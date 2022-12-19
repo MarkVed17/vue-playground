4eb2442363d10137567c1ae33ca9ef9482a35386
MarkVed17 <vedantlahane17@gmail.com>
2022-12-19 23:40:00 +0530

    added `Password Matcher App` to `Home View`

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-    <CounterApp />
+    <!-- <CounterApp /> -->
+    <PasswordMatcher />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 // import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-import CounterApp from '@/components/CounterApp.vue'
+// import CounterApp from '@/components/CounterApp.vue'
+import PasswordMatcher from '@/components/PasswordMatcher.vue'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     // HelloWorld,
-    CounterApp
+    // CounterApp,
+    PasswordMatcher
   }
 })
 </script>
