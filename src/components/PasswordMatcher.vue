7c685990cc5d3aabeddd24e2a8885808cd7ab4d5
MarkVed17 <vedantlahane17@gmail.com>
2022-12-19 23:38:40 +0530

    feat: created `Password Matcher App`

---
+++ src/components/PasswordMatcher.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class="form-wrapper">
+        <label>
+            Enter Password
+            <input type="password" v-model="passwords.passwordOne" />
+        </label>
+        <p>{{ passwords.passwordOne }}</p>
+        <label>
+            Re-enter Password
+            <input type="password" v-model="passwords.passwordTwo" />
+        </label>
+        <p>{{ passwords.passwordTwo }}</p>
+    </form>
+    <h3>{{(passwords.passwordOne !== passwords.passwordTwo ? "Passwords don't match" : "")}}</h3>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const passwords = ref({
+  passwordOne: '',
+  passwordTwo: ''
+})
+</script>
+
+<style>
+.form-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+}
+</style>
