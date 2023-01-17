a5318a7e3a2892753de2d4b69a7d894876e7c360
MarkVed17 <vedantlahane17@gmail.com>
2023-01-17 16:33:50 +0530

    added `Tweet Box App` to `Home View`

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
     <!-- <CounterApp /> -->
     <!-- <PasswordMatcher /> -->
-    <DisableSubmit />
+    <!-- <DisableSubmit /> -->
+    <TweetBox />
   </div>
 </template>
 
@@ -13,7 +14,8 @@ import { defineComponent } from 'vue'
 // import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 // import CounterApp from '@/components/CounterApp.vue'
 // import PasswordMatcher from '@/components/PasswordMatcher.vue'
-import DisableSubmit from '@/components/DisableSubmit.vue'
+// import DisableSubmit from '@/components/DisableSubmit.vue'
+import TweetBox from '@/components/TweetBox.vue'
 
 export default defineComponent({
   name: 'HomeView',
@@ -21,7 +23,8 @@ export default defineComponent({
     // HelloWorld,
     // CounterApp,
     // PasswordMatcher,
-    DisableSubmit
+    // DisableSubmit,
+    TweetBox
   }
 })
 </script>
