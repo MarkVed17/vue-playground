5007e49ee4b9861114cd5c701100babc7c8f562d
MarkVed17 <vedantlahane17@gmail.com>
2023-01-17 14:59:28 +0530

    added `Disable Submit App` to `Home View`

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,8 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
     <!-- <CounterApp /> -->
-    <PasswordMatcher />
+    <!-- <PasswordMatcher /> -->
+    <DisableSubmit />
   </div>
 </template>
 
@@ -11,14 +12,16 @@
 import { defineComponent } from 'vue'
 // import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 // import CounterApp from '@/components/CounterApp.vue'
-import PasswordMatcher from '@/components/PasswordMatcher.vue'
+// import PasswordMatcher from '@/components/PasswordMatcher.vue'
+import DisableSubmit from '@/components/DisableSubmit.vue'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     // HelloWorld,
     // CounterApp,
-    PasswordMatcher
+    // PasswordMatcher,
+    DisableSubmit
   }
 })
 </script>
