d9c498d1bbee119e22f0fd8183b5ee9acffc9bed
MarkVed17 <vedantlahane17@gmail.com>
2023-01-17 14:58:39 +0530

    feat: created `Disable Submit` App

---
+++ src/components/DisableSubmit.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <label>Enter a strong password</label>
+        <input v-model="password" />
+        <button :disabled="isDisabled">Submit</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const password = ref('')
+
+const isDisabled = computed(() => {
+  if (password.value.length < 8) {
+    return true
+  } else return false
+})
+</script>
+
+<style>
+</style>
